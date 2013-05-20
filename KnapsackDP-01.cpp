e8f560db91ce4be3b5ff4d1bda952134153c9173
sourcecode123 <amuttur123@gmail.com>
2013-05-20 22:49:27 +0530

    2nd

--- KnapsackDP-01.cpp
+++ KnapsackDP-01.cpp
@@ -1,73 +1,59 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstdio>
-#define MAX 10000 
+#include <cstring>
+#include <vector>
 
 using namespace std;
 
-int W, N, X, Y, counter, temp, V[MAX][MAX], wt[MAX], val[MAX], arr[MAX];
-bool keep[MAX][MAX];
+#define SIZE 1000
+
+int  V[SIZE][SIZE], N, maxWt, wt[SIZE], val[SIZE];
+bool keep[SIZE][SIZE];
+vector < int > items;
 
 int main() {
+    int temp;
     
-    //cout << "Enter The Number Of Items And Max Weight Of Knapsack
-    cin >> N >> W;
-	
-    //Enter The Weight And Value Of Each Of The "N" Items
-	for (int i = 1; i <= N; i++)
-		cin >> wt[i] >> val[i];
-	for (int i = 0; i <= N; i++) {
-		for (int j = 1; j <= W; j++ ) {
-			if (i == 0) {
-				V[i][j] = 0;
-				keep[i][j] = 0;
-			}
-			else {
-				temp = val[i];
-				if (j - wt[i] > 0)
-					temp += V[i-1][j-wt[i]];
-				if (wt[i] <= j && temp > V[i-1][j]) {
-					keep[i][j] = 1;
-					V[i][j] = temp;
-				}
-				else {
-					keep[i][j] = 0;
-					V[i][j] = V[i-1][j];
-				}
-			}
-		}
-	}
-	X = N;
-	Y = W;
-	counter = 0;
-	
-    /*
-    Value Matrix
-	for (int i = 0; i <= N; i++) {
-		for (int j = 1; j <= W; j++)
-			printf("%-6d", V[i][j]);
-		cout << endl;
-	}
-	*/
+    cin >> N >> maxWt;
+    
+    for (int i = 1; i <= N; i++)
+        cin >> wt[i] >> val[i];
 
-	while (Y > 0 && X > 0) {
-		if (keep[X][Y]) {
-			arr[counter++] = X;
-			Y -= wt[X];
-			X--;
-		}
-		else
-			X--;
-	}
-	if (!counter)
-		cout << "No Solution For Knapsack." << endl;
-	else {
-		for (int i = counter-1; i >= 0; i--) {
-			cout << arr[i];
-			if (i != 0)
-				cout << ", ";
-		}
-		cout << " Are The Element(s) Of The Knapsack. Max Weight = " << V[N][W] << endl;
-	}
-	return 0;
+    memset(keep, 0, SIZE*SIZE*sizeof(bool));
+    memset(V, 0, SIZE*SIZE*sizeof(int)); 
+    
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= maxWt; j++) {
+            temp = val[i];
+            V[i][j] = V[i-1][j];
+
+            if (j - wt[i] < 0)
+                continue;
+            
+            temp += V[i-1][j - wt[i]];
+            if (temp > V[i-1][j]) {
+                V[i][j] = temp;
+                keep[i][j] = 1;
+            }
+        }
+    }
+    
+    for (int i = N, j = maxWt; i >= 0 && j >= 0; i--) {
+        if (keep[i][j] == 1) {
+            j -= wt[i];
+            items.push_back(i);
+        }
+    }
+    
+    if (!items.empty()) {
+        cout << "Maximum Value: " << V[N][maxWt] << endl;
+        cout << "Items Are : ";
+        for (int i = 0; i < items.size(); i++) {
+            cout << items[i] << " ";
+        }
+        cout << endl;
+    }
+    else
+        cout << "No Solution Is Possible" << endl;
+    
+    return 0;
 }
