b4b468311b91c225dce9f41c61f8febdca002453
sourcecode123 <amuttur123@gmail.com>
2013-05-22 11:55:44 +0530

    2nd

--- Horspool.cpp
+++ Horspool.cpp
@@ -33,7 +33,7 @@ int main() {
             return 0;
         }
         else
-            i += shiftTable[text[i-k]];
+            i += shiftTable[text[i]];
     }
     cout << "Not Found" << endl;
     return -1;
