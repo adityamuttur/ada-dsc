d00b256caf988c6f3cf4e7d0c8c3f30385155df9
sourcecode123 <amuttur123@gmail.com>
2013-05-22 11:51:01 +0530

    2nd

--- BinomialCoefficientDP.cpp
+++ BinomialCoefficientDP.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
-#include <algorithm>
-#include <cstdio>
-#include <cstdlib>
-
-#define MAX 20
 
 using namespace std;
 
-int coeff[MAX+1][MAX+2], N, M;
+#define SIZE 1000
 
 int main() {
-	cin >> N >> M;
-	coeff[0][0] = 1;
-	for (int i = 0; i <= N; i++) {
-		for (int j = 0; j <= i; j++) {
-				if (j == 0 || j == i)
-					coeff[i][j] = 1;
-				else
-					coeff[i][j] = coeff[i-1][j-1] + coeff[i-1][j];
-		}
-	}
-	/*
-	for (int i = 0; i <= N; i++) {
-		for (int p = 1; p <= N-i; p++)
-			cout << "  ";
-		for (int j = 0; j <= i; j++) {
-			cout << coeff[i][j] << "   ";
-		}
-		cout << endl;
-	}
-	*/
-	cout << coeff[N][M] << endl;
-	return 0;
+    int N, K, binCoeff[SIZE][SIZE];
+    
+    cin >> N >> K;
+    
+    binCoeff[0][0] = 1;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= i; j++) {
+            if (j == 0 || j == i)
+                binCoeff[i][j] = 1;
+            else
+                binCoeff[i][j] = binCoeff[i-1][j-1] + binCoeff[i-1][j];
+        }
+    }
+    
+    /* Printing The Pascal Triangle 
+    for (int i = 0; i <= N; i++) {
+        for (int j = 1; j <= N-i; j++)
+            cout << " ";
+        for (int j = 0; j <= i; j++)
+            cout << binCoeff[i][j] << " ";
+        cout << endl;
+    }
+    */
+    
+    cout << N << "C" << K << "=" << binCoeff[N][K] << endl;
+    
+    return 0;
 }
